89c6f46c6effbc238c1bcfd9070801792b0127bd
Tsubaki-mk5 <mnt525.k@icloud.com>
2022-09-28 14:43:46 +0900

    a

--- ゲーム制作/Game03/Project/GameProject/Game/BOSS.h
+++ ゲーム制作/Game03/Project/GameProject/Game/BOSS.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "../Base/Base.h"
+
+class Boss : public Base {
+private:
+	CImage m_img;
+	bool m_flip;
+public:
+	Boss(const CVector2D& p, bool flip);
+	void Update();
+	void Draw();
+	void Collision(Base* b);
+
+};
