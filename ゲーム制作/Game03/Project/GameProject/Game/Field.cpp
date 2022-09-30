a07e9a2b74d040d132596eb892d33032dd85ee2d
Tsubaki-mk5 <mnt525.k@icloud.com>
2022-09-30 15:00:49 +0900

    q

--- ゲーム制作/Game03/Project/GameProject/Game/Field.cpp
+++ ゲーム制作/Game03/Project/GameProject/Game/Field.cpp
@@ -1,12 +1,9 @@
 #include "Field.h"
 
 Field::Field() :Base(eType_Field) {
-	m_space = COPY_RESOURCE("space", CImage);
+	
 
 }
 void Field::Draw() {
-	float sc;
-	m_space.Draw();
-	sc = m_scroll.x / 4;
-	m_space.SetRect(sc, 0, sc + 1280, 720);
+	
 }
--- ゲーム制作/Game03/Project/GameProject/Game/Field.h
+++ ゲーム制作/Game03/Project/GameProject/Game/Field.h
@@ -3,7 +3,7 @@
 
 class Field : public Base {
 private:
-	CImage m_space;
+	
 
 public:
 	Field();
