483cbc1a8bff5f345c0d26b01293319166575ef0
Tsubaki-mk5 <mnt525.k@icloud.com>
2022-09-30 11:01:24 +0900

    a

--- ゲーム制作/Game03/Project/GameProject/Game/BOSS.cpp
+++ ゲーム制作/Game03/Project/GameProject/Game/BOSS.cpp
@@ -1,15 +1,25 @@
 #include "Boss.h"
-
+#include "AnimData.h"
 Boss::Boss(const CVector2D& p, bool flip) : Base(e_Type_Boss) {
-	
+	m_img = COPY_RESOURCE("Boss", CImage);
+	m_img.ChangeAnimation(0);
+	m_pos = p;
+	m_img.SetCenter(128, 244);
+	m_flip = flip;
+
 }
 
 void Boss::Update()
 {
+	m_img.ChangeAnimation(eAnimIdle);
+	m_img.UpdateAnimation();
 }
 
 void Boss::Draw()
 {
+	m_img.SetPos(m_pos);
+	m_img.SetFlipH(m_flip);
+	m_img.Draw();
 }
 
 void Boss::Collision(Base* b)
