58eb6c608e7583019bf0477d79b14e3e67267f83
Tsubaki-mk5 <mnt525.k@icloud.com>
2022-10-03 11:10:41 +0900

    a

---
+++ ゲーム制作 - コピー/Game03/Project/GameProject/Game/Field.cpp
@@ -0,0 +1,13 @@
+#include "Field.h"
+
+Field::Field() :Base(eType_Field) {
+
+	m_DOUKUTSU = COPY_RESOURCE("DOUKUTSU", CImage);
+
+}
+void Field::Draw() {
+	float sc;
+	m_DOUKUTSU.Draw();
+	sc = m_scroll.x / 4;
+	m_DOUKUTSU.SetRect(sc, 0, sc + 1280, 720);
+}
---
+++ ゲーム制作 - コピー/Game03/Project/GameProject/Game/Field.h
@@ -0,0 +1,14 @@
+#pragma once
+#include"../Base/Base.h"
+
+class Field : public Base {
+private:
+	CImage m_DOUKUTSU;
+
+public:
+	Field();
+
+	void Draw();
+
+};
+
