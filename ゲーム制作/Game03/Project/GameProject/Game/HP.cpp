6b9a7ef663beba001501c00f29d7b4c38b879d66
minamiiiiii <22051925@d.anabuki-college.net>
2022-10-07 11:19:25 +0900

    1

---
+++ ゲーム制作/Game03/Project/GameProject/Game/HP.cpp
@@ -0,0 +1,15 @@
+#include "HP.h"
+
+HP::HP() : Base(eType_HP) {
+	m_img.Load("Image/hp.png");
+	m_img.SetSize(150, 150);
+}
+
+void HP::Draw() {
+	
+	float s = Width * NowHp / HpMax;
+
+	m_img.SetPos(m_pos);
+	m_img.SetSize(110, 50);
+	m_img.Draw();
+}
