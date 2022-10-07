4aca62abfd43e391fd410883d209444e5f834b14
kikkawa <haromunaguramyo-ma@docomo.ne.jp>
2022-10-07 12:18:33 +0900

    a

--- ゲーム制作/Game03/Project/GameProject/Game/AnimData.h
+++ ゲーム制作/Game03/Project/GameProject/Game/AnimData.h
@@ -11,23 +11,10 @@ enum  {
 	eAnimAttack01End,
 	eAnimAttack02,
 	eAnimAttack02End,
-	//eAnimAttack03,
-	//eAnimAttack03End,
 	eAnimDamage,
 	eAnimDamageEnd,
-	//eAnimDeath,
 	eAnimDown,
 	eAnimDownGround,
-	//eAnimWakeUp,
-	//eAnimGuard,
-	//eAnimGuardEnd,
-	//eAnimCrash,
-	//eAnimCrashEnd,
-	//eAnimExtAttack01,
-	//eAnimExtAttack01End,
-	//eAnimExtAttack02,
-	//eAnimExtAttack02End,
-	//eAnimStep,
 };
 //プレイヤーのアニメーションデータ
 extern TexAnimData player_anim_data[];
