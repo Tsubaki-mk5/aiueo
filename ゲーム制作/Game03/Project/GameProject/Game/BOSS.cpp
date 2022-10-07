8920858f21c4aabb0360587f7f044fe1e717990f
Tsubaki-mk5 <mnt525.k@icloud.com>
2022-10-07 11:44:41 +0900

    a

--- ゲーム制作/Game03/Project/GameProject/Game/BOSS.cpp
+++ ゲーム制作/Game03/Project/GameProject/Game/BOSS.cpp
@@ -33,7 +33,7 @@ Boss::Boss(const CVector2D& p, bool flip) : Base(eType_Boss) {
 	m_img = COPY_RESOURCE("Boss", CImage);
 	m_img.ChangeAnimation(0);
 	m_pos = p;
-	m_img.SetCenter(128, 244);
+	m_img.SetCenter(75,75 );
 	m_rect = CRect(-32, -128, 32, 0);
 	m_flip = flip;
 	m_is_ground = false;
