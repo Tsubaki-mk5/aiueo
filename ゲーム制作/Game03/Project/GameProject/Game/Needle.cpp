871da1acc5564a1691e5179363fdaa751f7bcb02
kikkawa <haromunaguramyo-ma@docomo.ne.jp>
2022-10-19 14:48:03 +0900

    a

--- ゲーム制作/Game03/Project/GameProject/Game/Needle.cpp
+++ ゲーム制作/Game03/Project/GameProject/Game/Needle.cpp
@@ -5,9 +5,9 @@ Needle::Needle(const CVector2D& pos) : Base(eType_Needle) {
 
 	m_img = COPY_RESOURCE("Needle", CImage);
 	m_pos = pos;
-	m_img.SetSize(120,80);
-	m_img.SetCenter(0, 80);
-	m_rect = CRect(0, -80, 120, 0);
+	m_img.SetSize(100,60);
+	m_img.SetCenter(50, 60);
+	m_rect = CRect(-30, -30, 30, 0);
 }
 void Needle::Update() {
 }
