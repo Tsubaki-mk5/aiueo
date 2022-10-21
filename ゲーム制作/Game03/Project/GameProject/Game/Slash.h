16961b3ba879e13df7dc2a8613a444e1f6ba95c0
minamiiiiii <22051925@d.anabuki-college.net>
2022-10-21 11:01:55 +0900

    a

--- ゲーム制作/Game03/Project/GameProject/Game/Slash.h
+++ ゲーム制作/Game03/Project/GameProject/Game/Slash.h
@@ -6,11 +6,11 @@ private:
 	//画像オブジェクト
 	CImage m_img;
 	//反転フラグ
-	bool	m_flip;
+	bool m_flip;
 	//攻撃番号
 	int m_attack_no;
 public:
-	Slash(const CVector2D& pos, bool flip, int attack_no);
+	Slash(const CVector2D& pos, bool flip, int type, int attack_no);
 	void Update();
 	void Draw();
 	int GetAttackNo() {
